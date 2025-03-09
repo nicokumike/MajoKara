bb2136531d548c8a042d8f1572fc227e998ac675
kinteyoi <59457560+kinteyoi@users.noreply.github.com>
2025-03-08 18:49:12 -0600

    camera beginnings

--- UI/camera_base.gd
+++ UI/camera_base.gd
@@ -1,1 +1,14 @@
 extends Camera2D
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			pass
+			# zoom in
+			#if event.button_index == zoom_in:
+				#zoom_pos = get_global_mouse_position()
+				# call the zoom function
+			# zoom out
+			#if event.button_index == zoom_out:
+				#zoom_pos = get_global_mouse_position()
+				# call the zoom function
