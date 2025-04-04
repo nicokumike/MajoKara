55d15c235eb0145dee6c2da5876e6a0f5d4e66c3
kinteyoi <calebazlin5@gmail.com>
2025-04-03 22:29:48 -0500

    grass growth

--- Entities/Environment/planter.gd
+++ Entities/Environment/planter.gd
@@ -1,17 +1,19 @@
 extends PathFollow2D
 
 @onready var planter = $"."
-@onready var PLANT = preload("res://Entities/Plants/bulb_plant.tscn")
+@onready var PLANT = preload("res://Entities/Plants/magic_grass.tscn")
 @onready var ground = $"../.."
 var counted = 0
+var max_plant_spawn = 500
 @onready var count = $"../../Count"
 
 func _on_timer_timeout():
-	var plant_point = randf_range(0, 1)
-	planter.progress_ratio = plant_point
-	var to_be_plant = PLANT.instantiate()
-	ground.add_child(to_be_plant)
-	to_be_plant.global_position = planter.global_position
-	counted += 1
-	count.text = str(counted)
-	
+	if counted <= max_plant_spawn:
+		var plant_point = randf_range(0, 1)
+		planter.progress_ratio = plant_point
+		var to_be_plant = PLANT.instantiate()
+		ground.add_child(to_be_plant)
+		to_be_plant.global_position = planter.global_position
+		counted += 1
+	else:
+		pass
---
+++ Entities/Plants/magic_grass.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@onready var line_2d: Line2D = $Line2D
+@onready var grow_clock: Timer = $GrowClock
+
+var growth_factor = 1
+var growth_amnt = 0
+var maximum_growth = 0
+var value = 0
+var max_plant_cap
+
+func _ready() -> void:
+	var cap_chance = randf_range(0, 1)
+	if cap_chance > .99:
+		max_plant_cap = -40
+	elif cap_chance > .95:
+		max_plant_cap = -20
+	elif cap_chance > .84:
+		max_plant_cap = -10
+	elif cap_chance > .71:
+		max_plant_cap = -7
+	elif cap_chance > .50:
+		max_plant_cap = -5
+	else:
+		max_plant_cap = -2
+	var max_growth_copy = randi_range(-1, max_plant_cap)
+	maximum_growth = max_growth_copy
+	grow_clock.start()
+	
+func grow(growth_factor):
+	growth_amnt += growth_factor
+	if growth_amnt > maximum_growth:
+		line_2d.set_point_position(0, Vector2(0, growth_amnt))
+	else:
+		grow_clock.stop()
+
+func _on_grow_clock_timeout() -> void:
+	grow_clock.wait_time = randf_range(0, 1.2)
+	grow(randi_range(-1, -2))
